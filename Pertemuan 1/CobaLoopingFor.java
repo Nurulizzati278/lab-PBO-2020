b9e48db1508533369a01e40ad7d4997d42138cfe
Nurul izzati <yuyunnurul7@gmail.com>
2020-10-15 23:35:33 +0700

    Membuat Kode Pertemuan 1

--- Pertemuan 1/CobaLoopingFor.java
+++ Pertemuan 1/CobaLoopingFor.java
@@ -13,6 +13,6 @@ public class CobaLoopingFor
 /*penjelasan program diatas
 	program diatas digunakan untuk mecoba looping for, dimana perulangan for dipakai saat melakukan perulangan 
 	dengan jumlah yang sudah diketahui. Pada code diatas perulangan int i akan dilakukan 10 kali dari 0 sampai 9
-	dan akan mencetak * secara vertikal dari atas kebawah, sedangkan int j akan melakukan perulangan j<i+1 
+	dan akan mencetak * secara vertikal dari atas kebawah, sedangkan int j akan melakukan perulangan sampai j<i+1 
 	dan akan mencetak * secara horzontal. Perulangan dilakukan sampai semua kondisi telah terpenuhi.
 */
---
+++ pertemuan 2/CopyArray2.java
@@ -0,0 +1,12 @@
+public  class  CopyArray3 {
+ 	public  static  void  main ( String  args []) {
+ 
+		String str1 =  " Informatika Universitas Syiah Kuala " ;
+		Sistem . keluar . println (str1 . beginWith ( " Inf " ));
+		Sistem . keluar . println (str1 . endingWith ( " kuala " ));
+		Sistem . keluar . println (str1 . beginWith ( " Inf " , 1 ));
+		Sistem . keluar . println (str1 . beginWith ( " Inf " , 2 ));
+		Sistem . keluar . println (str1 . beginWith ( " Inf " , 3 ));
+
+	}
+}
